98eb2c302de835aaf8753302e046387ab01051d9
Minh Huynh <minhh1990@gmail.com>
2017-01-03 20:21:34 -0800

    First half

--- spec/models/categories_spec.rb
+++ spec/models/categories_spec.rb
@@ -1,8 +1,5 @@
 require 'spec_helper'
 
 describe Category do
-  it "creates a new category" do 
-    category = Category.create(title: "Action Movies")
-    expect(Category.first.title).to eq("Action Movies")
-  end
+  it {should have_many(:videos)}
 end
