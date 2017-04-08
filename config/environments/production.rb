347f5b15e496fead43f9e1da82a455b6b158d5fb
Minh Huynh <minhh1990@gmail.com>
2017-04-08 09:11:06 -0700

    changed production email configurations to use heroku mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,11 +21,11 @@ Myflix::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV['gmail_username'],
-    password:             ENV['gmail_password'],
+    address:              'ENV['MAILGUN_SMTP_SERVER'],
+    port:                 ENV['MAILGUN_SMTP_PORT'],
+    domain:               'morning-castle-51455.herokuapp.com',
+    user_name:            ENV['MAILGUN_SMTP_LOGIN'],
+    password:             ENV['MAILGUN_SMTP_PASSWORD'],
     authentication:       'plain',
-    enable_starttls_auto: true  }
-end
+  }
+  ActionMailer::Base.delivery_method = :smtp
