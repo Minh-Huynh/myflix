4ce1eaf76c15b73c6a3070505ce5e34c0b5b0724
Minh Huynh <minhh1990@gmail.com>
2017-04-11 18:15:13 -0700

    removed a test

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,12 +1,4 @@
-class PasswordResetsController < ApplicationController
-
-  def show
-    @user = User.find_by_reset_token(params[:id])
-    redirect_to expired_token_path unless @user
-  end
-
-  def create
-    user = User.find_by(reset_token: params[:reset_token]) || User.find(params[:user_id])
+= User.find_by(reset_token: params[:reset_token])
     if user
       user.password = params[:password]
       user.generate_reset_token
