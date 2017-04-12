050a9f3dc4a9199799754d857c1042ca0a8c2b5c
Minh Huynh <minhh1990@gmail.com>
2017-04-11 18:39:48 -0700

    accidentally deleted half of password reset controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,12 @@
-= User.find_by(reset_token: params[:reset_token])
+class PasswordResetsController < ApplicationController
+
+  def show
+    @user = User.find_by_reset_token(params[:id])
+    redirect_to expired_token_path unless @user
+  end
+
+  def create
+    user= User.find_by(reset_token: params[:reset_token])
     if user
       user.password = params[:password]
       user.generate_reset_token
@@ -9,5 +17,4 @@
       redirect_to expired_token_path
     end
   end
-
 end
