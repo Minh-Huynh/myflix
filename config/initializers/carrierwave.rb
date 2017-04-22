4f78acee6e275ff3bc63f4ceda896c2267df39b2
Minh Huynh <minhh1990@gmail.com>
2017-04-22 10:08:30 -0700

    changed credentials to only be in initializers/carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -17,7 +17,7 @@ CarrierWave.configure do |config|
       config.aws_acl = 'public-read'
       config.aws_credentials = {
         :access_key_id      => ENV.fetch('AWS_ACCESS_KEY_ID'),            
-        :secret_access_key  => ENV.fetch('AWS-SECRET_ACCESS_KEY'),
+        :secret_access_key  => ENV.fetch('AWS_SECRET_ACCESS_KEY'),
         :region             => ENV.fetch('AWS_REGION')
 
       }
